8e817c41b1700d8a04ef706ed4d9194f5eef08ac
Absozero <aswsub123@gmail.com>
2024-04-28 03:29:09 -0700

    feat(cwd): Completed cwd switching feature integrated with nvim-tree

--- lua/abso/plugins/tree.lua
+++ lua/abso/plugins/tree.lua
@@ -6,13 +6,26 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   opts =  {
-    disable_netrw = true,
-    hijack_netrw = true,
-   hijack_cursor = true,
-   sync_root_with_cwd = true,
+    
   },
   config = function()
-    require("nvim-tree").setup {}
+    require("nvim-tree").setup {
+    disable_netrw = true,
+    hijack_netrw = true,
+    hijack_cursor = true,
+    actions = {
+    change_dir = {
+	global = true,
+    },
+    },
+    update_focused_file = {
+      enable = true,
+      update_root = true,
+    },
+    sync_root_with_cwd = true,
+    reload_on_bufenter = true,
+    respect_buf_cwd = true
+    }
   end,
 }
 
